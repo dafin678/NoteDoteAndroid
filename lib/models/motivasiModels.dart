bd639f099583b5545493158dcb3327a6f35ccb86
Danang Widyorukmantiyoro <danang.widyorukmantiyoro@ui.ac.id>
2021-12-31 21:32:22 +0700

    motivasii

--- lib/models/motivasiModels.dart
+++ lib/models/motivasiModels.dart
@@ -1,12 +1,15 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe, avoid_unnecessary_containers, avoid_print, file_names, unused_import, prefer_const_constructors
+
 import 'dart:convert';
 
+// ignore: non_constant_identifier_names
 List<Motivasi> IsiPesanFromJson(String str) =>
     List<Motivasi>.from(json.decode(str).map((x) => Motivasi.fromJson(x)));
 
+// ignore: non_constant_identifier_names
 String IsiPesanToJson(List<Motivasi> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
-// ignore: file_names
 class Motivasi {
   String model;
   int pk;
@@ -21,12 +24,10 @@ class Motivasi {
       );
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['model'] = this.model;
-    data['pk'] = this.pk;
-    if (fields != null) {
-      data['fields'] = this.fields.toJson();
-    }
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['model'] = model;
+    data['pk'] = pk;
+    data['fields'] = fields.toJson();
     return data;
   }
 }
@@ -41,9 +42,9 @@ class Fields {
       Fields(to: json['to'], pesan: json['pesan']);
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
+    final Map<String, dynamic> data = <String, dynamic>{};
     data['to'] = to;
-    data['pesan'] = this.pesan;
+    data['pesan'] = pesan;
     return data;
   }
 }
