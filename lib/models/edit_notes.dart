26c07c448ebb0e55f63e2ea16cda4dd3ffb04d85
Rizky Juniastiar <72258623+rizkyyjun@users.noreply.github.com>
2021-12-31 22:17:37 +0700

    fix warning

--- lib/models/edit_notes.dart
+++ lib/models/edit_notes.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import "package:flutter/widgets.dart";
+// import "package:flutter/widgets.dart";
 import 'package:projek_akhir_f07/models/notes_collection.dart';
 import 'package:projek_akhir_f07/models/notes_model.dart';
 import 'package:provider/provider.dart';
