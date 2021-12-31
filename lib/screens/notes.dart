ce544255ffac1cb7647407538a628aa2e6ba8547
Rizky Juniastiar <rizkyjuniastiar37@gmail.com>
2021-12-31 22:21:29 +0700

    fix warning

--- lib/screens/notes.dart
+++ lib/screens/notes.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+// import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:projek_akhir_f07/models/notes_collection.dart';
 import 'package:projek_akhir_f07/models/notes_model.dart';
