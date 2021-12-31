e585eaf5ec354822e40cb216629db0581728562c
Rizky Juniastiar <rizkyjuniastiar37@gmail.com>
2021-12-31 18:47:33 +0700

    edit schedule screen

--- lib/screens/schedule_screen.dart
+++ lib/screens/schedule_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
+import '../models/weekly_schedule_models.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class MondayScreen extends StatefulWidget {
   static const routeName = '/monday-screen';
@@ -11,6 +14,71 @@ class MondayScreen extends StatefulWidget {
 }
 
 class _MondayScreen extends State<MondayScreen>{
+
+  static List<WeeklySchedule> extractedData = [];
+
+  Future<void> getData() async {
+    var client = http.Client();
+    WeeklySchedule weeklyScheduleModel;
+    const url = 'https://notedote.herokuapp.com/weekly_schedule/get-monday-schedule/';
+
+    try {
+      // print('coba');
+      var response = await client.get(Uri.parse(url));
+      if (response.statusCode == 200){
+        // print('success!');
+        // print(response.body);
+        var jsonString = response.body;
+        var jsonRes = jsonDecode(jsonString);
+        var jumlahData = jsonRes.length;
+        print(jumlahData);
+        for (var i = 0; i < jumlahData; ++i){
+          var jsonMap = json.decode(jsonString)[i];
+          print('isi json index ke - ${i}');
+          print(jsonMap);
+          weeklyScheduleModel = WeeklySchedule.fromJson(jsonMap);
+          extractedData.add(weeklyScheduleModel);
+        }
+
+
+      }
+    } catch (exception){
+      // print(exception);
+      // return extractedData;
+    }
+
+    // return extractedData;
+  }
+
+  // String strDay(int intDay){
+  //   String res = '';
+  //
+  //   switch (intDay){
+  //     case 1:
+  //       res = "monday";
+  //       break;
+  //     case 2:
+  //       res = "tuesday";
+  //       break;
+  //     case 3:
+  //       res = "wednesday";
+  //       break;
+  //     case 4:
+  //       res = "thursday";
+  //       break;
+  //     case 5:
+  //       res = "friday";
+  //       break;
+  //     case 6:
+  //       res = "saturday";
+  //       break;
+  //     case 7:
+  //       res = "sunday";
+  //       break;
+  //
+  //     return res;
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +95,68 @@ class _MondayScreen extends State<MondayScreen>{
         ],
       ),
       drawer: const MainDrawer(),
+      body: Container(
+        child: FutureBuilder(
+          future: getData(),
+          builder: (context, snapshot){
+            if (snapshot.hasData){
+              return ListView.builder(
+                itemCount: extractedData.length,
+                itemBuilder: (context, index){
+                  var schedule = extractedData[index];
+                  return Container(
+                    height: 100,
+                    margin: const EdgeInsets.all(8),
+                    child: Flexible(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            schedule.fields.name,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          // Text(
+                          //   schedule.fields.day
+                          // ),
+                          Text(
+                            schedule.fields.startTime
+                          ),
+                          Text(
+                            schedule.fields.dueTime
+                          )
+                        ],
+                      ),
+                    )
+                  );
+                },
+              );
+
+              // return Column(
+              //   children: extractedData.map((i) {
+              //     return ScheduleTable(
+              //
+              //     )
+              //   }),
+              // )
+
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
+
+      // body: Center(
+      //   child: ElevatedButton(
+      //     child: const Text("Get Data Test"),
+      //     onPressed: getData,
+      //   ),
+      // ),
     );
   }
 
@@ -43,6 +173,41 @@ class TuesdayScreen extends StatefulWidget {
 
 class _TuesdayScreen extends State<TuesdayScreen>{
 
+  static List<WeeklySchedule> extractedData = [];
+
+  Future<void> getData() async {
+    var client = http.Client();
+    WeeklySchedule weeklyScheduleModel;
+    const url = 'https://notedote.herokuapp.com/weekly_schedule/get-tuesday-schedule/';
+
+    try {
+      // print('coba');
+      var response = await client.get(Uri.parse(url));
+      if (response.statusCode == 200){
+        // print('success!');
+        // print(response.body);
+        var jsonString = response.body;
+        var jsonRes = jsonDecode(jsonString);
+        var jumlahData = jsonRes.length;
+        print(jumlahData);
+        for (var i = 0; i < jumlahData; ++i){
+          var jsonMap = json.decode(jsonString)[i];
+          print('isi json index ke - ${i}');
+          print(jsonMap);
+          weeklyScheduleModel = WeeklySchedule.fromJson(jsonMap);
+          extractedData.add(weeklyScheduleModel);
+        }
+
+
+      }
+    } catch (exception){
+      // print(exception);
+      // return extractedData;
+    }
+
+    // return extractedData;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,6 +223,12 @@ class _TuesdayScreen extends State<TuesdayScreen>{
         ],
       ),
       drawer: const MainDrawer(),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Get Data Test"),
+          onPressed: getData,
+        ),
+      ),
     );
   }
 
@@ -74,6 +245,41 @@ class WednesdayScreen extends StatefulWidget {
 
 class _WednesdayScreen extends State<WednesdayScreen>{
 
+  static List<WeeklySchedule> extractedData = [];
+
+  Future<void> getData() async {
+    var client = http.Client();
+    WeeklySchedule weeklyScheduleModel;
+    const url = 'https://notedote.herokuapp.com/weekly_schedule/get-wednesday-schedule/';
+
+    try {
+      // print('coba');
+      var response = await client.get(Uri.parse(url));
+      if (response.statusCode == 200){
+        // print('success!');
+        // print(response.body);
+        var jsonString = response.body;
+        var jsonRes = jsonDecode(jsonString);
+        var jumlahData = jsonRes.length;
+        print(jumlahData);
+        for (var i = 0; i < jumlahData; ++i){
+          var jsonMap = json.decode(jsonString)[i];
+          print('isi json index ke - ${i}');
+          print(jsonMap);
+          weeklyScheduleModel = WeeklySchedule.fromJson(jsonMap);
+          extractedData.add(weeklyScheduleModel);
+        }
+
+
+      }
+    } catch (exception){
+      // print(exception);
+      // return extractedData;
+    }
+
+    // return extractedData;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -89,6 +295,12 @@ class _WednesdayScreen extends State<WednesdayScreen>{
         ],
       ),
       drawer: const MainDrawer(),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Get Data Test"),
+          onPressed: getData,
+        ),
+      ),
     );
   }
 
@@ -105,6 +317,41 @@ class ThursdayScreen extends StatefulWidget {
 
 class _ThursdayScreen extends State<ThursdayScreen>{
 
+  static List<WeeklySchedule> extractedData = [];
+
+  Future<void> getData() async {
+    var client = http.Client();
+    WeeklySchedule weeklyScheduleModel;
+    const url = 'https://notedote.herokuapp.com/weekly_schedule/get-thursday-schedule/';
+
+    try {
+      // print('coba');
+      var response = await client.get(Uri.parse(url));
+      if (response.statusCode == 200){
+        // print('success!');
+        // print(response.body);
+        var jsonString = response.body;
+        var jsonRes = jsonDecode(jsonString);
+        var jumlahData = jsonRes.length;
+        print(jumlahData);
+        for (var i = 0; i < jumlahData; ++i){
+          var jsonMap = json.decode(jsonString)[i];
+          print('isi json index ke - ${i}');
+          print(jsonMap);
+          weeklyScheduleModel = WeeklySchedule.fromJson(jsonMap);
+          extractedData.add(weeklyScheduleModel);
+        }
+
+
+      }
+    } catch (exception){
+      // print(exception);
+      // return extractedData;
+    }
+
+    // return extractedData;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -120,6 +367,12 @@ class _ThursdayScreen extends State<ThursdayScreen>{
         ],
       ),
       drawer: const MainDrawer(),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Get Data Test"),
+          onPressed: getData,
+        ),
+      ),
     );
   }
 
@@ -136,6 +389,41 @@ class FridayScreen extends StatefulWidget {
 
 class _FridayScreen extends State<FridayScreen>{
 
+  static List<WeeklySchedule> extractedData = [];
+
+  Future<void> getData() async {
+    var client = http.Client();
+    WeeklySchedule weeklyScheduleModel;
+    const url = 'https://notedote.herokuapp.com/weekly_schedule/get-friday-schedule/';
+
+    try {
+      // print('coba');
+      var response = await client.get(Uri.parse(url));
+      if (response.statusCode == 200){
+        // print('success!');
+        // print(response.body);
+        var jsonString = response.body;
+        var jsonRes = jsonDecode(jsonString);
+        var jumlahData = jsonRes.length;
+        print(jumlahData);
+        for (var i = 0; i < jumlahData; ++i){
+          var jsonMap = json.decode(jsonString)[i];
+          print('isi json index ke - ${i}');
+          print(jsonMap);
+          weeklyScheduleModel = WeeklySchedule.fromJson(jsonMap);
+          extractedData.add(weeklyScheduleModel);
+        }
+
+
+      }
+    } catch (exception){
+      // print(exception);
+      // return extractedData;
+    }
+
+    // return extractedData;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -151,6 +439,12 @@ class _FridayScreen extends State<FridayScreen>{
         ],
       ),
       drawer: const MainDrawer(),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Get Data Test"),
+          onPressed: getData,
+        ),
+      ),
     );
   }
 
@@ -167,6 +461,41 @@ class SaturdayScreen extends StatefulWidget {
 
 class _SaturdayScreen extends State<SaturdayScreen>{
 
+  static List<WeeklySchedule> extractedData = [];
+
+  Future<void> getData() async {
+    var client = http.Client();
+    WeeklySchedule weeklyScheduleModel;
+    const url = 'https://notedote.herokuapp.com/weekly_schedule/get-saturday-schedule/';
+
+    try {
+      // print('coba');
+      var response = await client.get(Uri.parse(url));
+      if (response.statusCode == 200){
+        // print('success!');
+        // print(response.body);
+        var jsonString = response.body;
+        var jsonRes = jsonDecode(jsonString);
+        var jumlahData = jsonRes.length;
+        print(jumlahData);
+        for (var i = 0; i < jumlahData; ++i){
+          var jsonMap = json.decode(jsonString)[i];
+          print('isi json index ke - ${i}');
+          print(jsonMap);
+          weeklyScheduleModel = WeeklySchedule.fromJson(jsonMap);
+          extractedData.add(weeklyScheduleModel);
+        }
+
+
+      }
+    } catch (exception){
+      // print(exception);
+      // return extractedData;
+    }
+
+    // return extractedData;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -182,6 +511,12 @@ class _SaturdayScreen extends State<SaturdayScreen>{
         ],
       ),
       drawer: const MainDrawer(),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Get Data Test"),
+          onPressed: getData,
+        ),
+      ),
     );
   }
 
@@ -198,6 +533,41 @@ class SundayScreen extends StatefulWidget {
 
 class _SundayScreen extends State<SundayScreen>{
 
+  static List<WeeklySchedule> extractedData = [];
+
+  Future<void> getData() async {
+    var client = http.Client();
+    WeeklySchedule weeklyScheduleModel;
+    const url = 'https://notedote.herokuapp.com/weekly_schedule/get-sunday-schedule/';
+
+    try {
+      // print('coba');
+      var response = await client.get(Uri.parse(url));
+      if (response.statusCode == 200){
+        // print('success!');
+        // print(response.body);
+        var jsonString = response.body;
+        var jsonRes = jsonDecode(jsonString);
+        var jumlahData = jsonRes.length;
+        print(jumlahData);
+        for (var i = 0; i < jumlahData; ++i){
+          var jsonMap = json.decode(jsonString)[i];
+          print('isi json index ke - ${i}');
+          print(jsonMap);
+          weeklyScheduleModel = WeeklySchedule.fromJson(jsonMap);
+          extractedData.add(weeklyScheduleModel);
+        }
+
+
+      }
+    } catch (exception){
+      // print(exception);
+      // return extractedData;
+    }
+
+    // return extractedData;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -213,6 +583,12 @@ class _SundayScreen extends State<SundayScreen>{
         ],
       ),
       drawer: const MainDrawer(),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Get Data Test"),
+          onPressed: getData,
+        ),
+      ),
     );
   }
 
